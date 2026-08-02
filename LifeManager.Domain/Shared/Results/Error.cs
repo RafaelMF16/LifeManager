985a4f18a44ca78cd49997f18ab830d7e8a2238d
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2026-08-02 20:30:27 -0300

    Adding result pattern

---
+++ LifeManager.Domain/Shared/Results/Error.cs
@@ -0,0 +1,28 @@
+﻿namespace LifeManager.Domain.Shared.Results
+{
+    public record Error
+    {
+        public string Code { get; }
+        public string Message { get; }
+        public ErrorType Type { get; }
+
+        private Error(string code, string message, ErrorType type)
+        {
+            Code = code;
+            Message = message;
+            Type = type;
+        }
+
+        public static Error Validation(string code, string message)
+            => new(code, message, ErrorType.Validation);
+
+        public static Error NotFound(string code, string message)
+            => new(code, message, ErrorType.NotFound);
+
+        public static Error Unauthorized(string code, string message)
+            => new(code, message, ErrorType.Unauthorized);
+
+        public static Error Failure(string code, string message)
+            => new(code, message, ErrorType.Failure);
+    }
+}
---
+++ LifeManager.Domain/Shared/Results/ErrorType.cs
@@ -0,0 +1,11 @@
+﻿namespace LifeManager.Domain.Shared.Results
+{
+    public enum ErrorType
+    {
+        None = 0,
+        Validation = 1,
+        NotFound = 2,
+        Unauthorized = 3,
+        Failure = 4,
+    }
+}
