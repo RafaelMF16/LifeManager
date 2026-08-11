86cf3cdcd0f901894c495deec71892111a98d2f9
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2026-08-11 19:18:32 -0300

    Add DI in infra

---
+++ LifeManager.Infrastructure/DI/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using LifeManager.Domain.Auth.Interfaces;
+using LifeManager.Domain.Users.Interfaces;
+using LifeManager.Infrastructure.Auth;
+using LifeManager.Infrastructure.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LifeManager.Infrastructure.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ LifeManager.Infrastructure/Users/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using LifeManager.Domain.Users;
+using LifeManager.Domain.Users.Interfaces;
+
+namespace LifeManager.Infrastructure.Users
+{
+    public class UserRepository : IUserRepository
+    {
+        public User Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User? GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
