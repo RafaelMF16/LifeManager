b4ef53b0392576a9261ef7e26c9c9238f40ac0ec
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2026-08-12 20:42:26 -0300

    DbContext has been implemented

---
+++ LifeManager.Infrastructure/Postgres/Configurations/UserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using LifeManager.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LifeManager.Infrastructure.Postgres.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            
+        }
+    }
+}
---
+++ LifeManager.Infrastructure/Postgres/LifeManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LifeManager.Domain.Auth;
+using LifeManager.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace LifeManager.Infrastructure.Postgres
+{
+    public class LifeManagerDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LifeManagerDbContext).Assembly);
+        }
+    }
+}
